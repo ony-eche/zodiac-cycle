f77eb176c8d2a424e800cb863ec79304275d2017
comfort elaigwu <elaigwuonyeche@gmail.com>
2026-03-23 07:16:57 +0100

    fix vercel routing with explicit static file routes

--- dist/assets/moon-Bi5lOjhW.js
+++
@@ -1,6 +0,0 @@
-import{c as o}from"./createLucideIcon-DqV5KeLS.js";/**
- * @license lucide-react v0.487.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const c=[["path",{d:"M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z",key:"a7tn18"}]],t=o("moon",c);export{t as M};
